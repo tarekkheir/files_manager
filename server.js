aab97c81d1832fc0b77733aa00baa683084a6b50
Tarek <kheirtarek@hotmail.fr>
2022-01-19 22:28:36 +0100

    task 2

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log('Server running on port 5000');
+  console.log('Server running on port 5000');
 });
 
 app.use('/', router);
+
+module.exports = app;
