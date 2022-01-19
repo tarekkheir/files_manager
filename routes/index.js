c1321ca50b25cf7eb1d5c9fb2b5e02963f582b2b
Tarek <kheirtarek@hotmail.fr>
2022-01-20 00:53:49 +0100

    fix index

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,8 @@
 import AppControllers from '../controllers/AppControllers';
 const router = require('express').Router();
 
-router.get('/status', (req, res) => {
-  AppControllers.getStatus(req, res);
-});
+router.get('/status', AppControllers.getStatus);
 
-router.get('/stats', (req, res) => {
-  AppControllers.getStats(req, res);
-});
+router.get('/stats', AppControllers.getStats);
 
 module.exports = router;
