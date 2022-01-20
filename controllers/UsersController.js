33c3e5604f48c0cd04b8171c4dac8963c0a89c1e
Tarek <kheirtarek@hotmail.fr>
2022-01-20 23:35:15 +0100

    all files

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,6 +4,7 @@ import { createHash } from 'crypto';
 class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
+
     if (!email) return res.status(400).send({ error: 'Missing email' });
     if (!password) return res.status(400).send({ error: 'Missing password' });
 
@@ -12,7 +13,6 @@ class UsersController {
     if (await users.findOne({ email })) return res.status(400).send({ error: 'Already exist' });
 
     const hash = createHash('sha1').update(password);
-
     const user = await users.insertOne({ email, password: hash.digest('hex') });
 
     return res.status(201).send({ id: user.insertedId, email });
