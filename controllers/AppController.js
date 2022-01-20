e7579d7b85813a853883112ec5bd32f42e464493
Tarek <kheirtarek@hotmail.fr>
2022-01-20 01:03:47 +0100

    fix controllers

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,18 +1,18 @@
-import RedisClient from '../utils/redis';
-import DBClient from '../utils/db';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 class AppController {
   static getStatus(req, res) {
     return res.status(200).send({
-      'redis': RedisClient.isAlive(),
-      'db': DBClient.isAlive()
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive()
     });
   }
 
   static async getStats(req, res) {
     return res.status(200).send({
-      'users': await DBClient.nbUsers(),
-      'files': await DBClient.nbFiles()
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles()
     });
   }
 }
