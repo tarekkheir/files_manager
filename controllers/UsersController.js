54a553a95a8edefb1f18d529b7ce8c9d4bd6be52
Tarek <kheirtarek@hotmail.fr>
2022-01-21 12:35:33 +0100

    task 3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,45 +1,26 @@
-import { createHash } from 'crypto';
 import dbClient from '../utils/db';
+import sha1 from 'sha1';
 
 class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
-    const users = await dbClient.db.collection('users');
 
     if (!email) return res.status(400).send({ error: 'Missing email' });
     if (!password) return res.status(400).send({ error: 'Missing password' });
 
-    const existingUser = await users.findOne({ email });
-    if (existingUser) return res.status(400).send({ error: 'Already exist' });
+    const users = await dbClient.db.collection('users');
 
-    const hashedPwd = createHash('sha1').update(password).digest('hex');
-    const newUserData = { email, password: hashedPwd };
-    const result = await users.insertOne(newUserData);
+    if (await users.findOne({ email })) return res.status(400).send({ error: 'Already exist' });
 
-    return res.status(201).send({ id: result.insertedId, email });
+    let user;
+    try {
+      user = await users.insertOne({ email, password: sha1(password) });
+    } catch (err) {
+      return res.status(400).send({ error: `DB insert failed: ${err}` });
+    }
+
+    return res.status(201).send({ id: user.insertedId, email });
   }
 }
 
-export default UsersController;
-// import dbClient from '../utils/db';
-// import { createHash } from 'crypto';
-
-// class UsersController {
-//   static async postNew(req, res) {
-//     const { email, password } = req.body;
-
-//     if (!email) return res.status(400).send({ error: 'Missing email' });
-//     if (!password) return res.status(400).send({ error: 'Missing password' });
-
-//     const users = await dbClient.db.collection('users');
-
-//     if (await users.findOne({ email })) return res.status(400).send({ error: 'Already exist' });
-
-//     const hash = createHash('sha1').update(password);
-//     const user = await users.insertOne({ email, password: hash.digest('hex') });
-
-//     return res.status(201).send({ id: user.insertedId, email });
-//   }
-// }
-
-// module.exports = UsersController;
+module.exports = UsersController;
