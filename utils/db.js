0476c1f722a2fbeabcfc56777ba7d8b29fe30dfe
Tarek <kheirtarek@hotmail.fr>
2022-01-21 13:47:08 +0100

    task 3

--- utils/db.js
+++ utils/db.js
@@ -6,7 +6,7 @@ class DBClient {
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
 
-    MongoClient.connect(new ServerApiVersion(host, port))
+    MongoClient.connect(new Server(host, port))
       .then((client) => { this.db = client.db(database); });
   }
 
